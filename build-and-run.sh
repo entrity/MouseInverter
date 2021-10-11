ed18872894974fdab524f09553e289eddc049bdb
Markham <quinesquines@gmail.com>
2021-10-11 08:32:44 -0700

    Update build script

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,14 +1,23 @@
 # Build
-cd MouseInverter && \
-/mnt/c/windows/system32/cmd.exe /c \
-'C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe' \
-MouseInverter.csproj
+# cd MouseInverter && \
+# pwd && \
 
-# Check success
-ret=$?
-echo ret $ret
+THISFILE=`readlink -f "$0"`
+THISDIR=`dirname "${THISFILE}"`
+PROJFILE="$THISDIR/MouseInverter/MouseInverter.csproj"
+TOOLPATH='C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe'
 
-# Run
-if !(($ret)); then
-  bin/Debug/MouseInverter.exe
-fi
+function build () {
+  rsync -r "$THISDIR" ~/win/home/Desktop
+  cd ~/win/home/Desktop/MouseInverter/MouseInverter
+  printf -v execpath "%q" "$TOOLPATH"
+  execpath=$(<<<"$execpath" sed 's@\(\\\([ ()]\)\)@`\2@g')
+  powershell.exe -Command "$execpath" "MouseInverter.csproj"
+}
+
+function run () {
+  ~/win/home/Desktop/MouseInverter/MouseInverter/bin/Debug/MouseInverter.exe
+}
+
+(($#)) || build # Any args makes "build" get skipped
+run
